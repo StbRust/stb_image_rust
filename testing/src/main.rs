77b655b31ad575229875ad13b3f816285aa388d3
Roman Shapiro <netframeworker@gmail.com>
2022-01-01 14:37:23 +0700

    Basic testing app

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,5 +1,6 @@
 extern crate image;
 extern crate stb_image;
+extern crate stb_image_rust;
 
 use std::fs;
 use std::fs::File;
@@ -8,12 +9,14 @@ use std::io::{prelude::*, Cursor};
 use std::time::Instant;
 
 use image::ImageFormat;
+use stb_image::image::LoadResult;
 
 fn main() {
     let paths = fs::read_dir(r"D:\Projects\Hebron\TestImages").unwrap();
 
     let mut loadTime1: u128 = 0;
     let mut loadTime2: u128 = 0;
+    let mut loadTime3: u128 = 0;
 
     for path2 in paths {
         let path = path2.unwrap().path();
@@ -47,30 +50,78 @@ fn main() {
         let mut y: i32 = 0;
         let mut comp: i32 = 0;
 
-        let mut buffer: Vec<u8>;
+        let img: *mut u8;
         unsafe {
-            let img = stb_image::stbi_load_from_memory(
+            img = stb_image_rust::stbi_load_from_memory(
                 contents.as_mut_ptr(),
                 contents.len() as i32,
                 &mut x,
                 &mut y,
                 &mut comp,
-                stb_image::STBI_rgb_alpha,
+                stb_image_rust::STBI_rgb_alpha,
             );
-            stb_image::c_runtime::free(img);
         }
 
         loadTime1 += now.elapsed().as_millis();
 
         let now2 = Instant::now();
 
-        let img2 = image::load_from_memory(&contents.as_mut());
+        let img2Wrapped =
+            stb_image::image::load_from_memory_with_depth(&contents.as_mut(), 4, true);
+
+        if let LoadResult::ImageU8(img2) = img2Wrapped {
+            'outer: loop {
+                if (x != img2.width as i32) {
+                    println!("width mismatch: {} {}", x, img2.width);
+                    break;
+                }
+
+                if (y != img2.height as i32) {
+                    println!("height mismatch: {} {}", y, img2.height);
+                    break;
+                }
+
+                let len = x * y * 4;
+                if (len != img2.data.len() as i32) {
+                    println!("len mismatch: {} {}", len, img2.data.len());
+                    break;
+                }
+
+                for i in 0..len - 1 {
+                    unsafe {
+                        if (*img.offset(i as isize) != img2.data[i as usize]) {
+                            println!(
+                                "data mismatch: {} {} {}",
+                                i,
+                                *img.offset(i as isize),
+                                img2.data[i as usize]
+                            );
+                            break 'outer;
+                        }
+                    }
+                }
+
+                break;
+            }
+        } else {
+            println!("image isnt ImageU8");
+        }
 
         loadTime2 += now2.elapsed().as_millis();
 
+        unsafe {
+            stb_image_rust::c_runtime::free(img);
+        }
+
+        let now3 = Instant::now();
+
+        let img3 = image::load_from_memory(&contents.as_mut());
+
+        loadTime3 += now3.elapsed().as_millis();
+
         println!(
-            "Width: {}, Height: {}, Comp: {}, Load Time: {} ms, Load Time 2: {}",
-            x, y, comp, loadTime1, loadTime2
+            "Width: {}, Height: {}, Comp: {}, Load Time: {} ms, Load Time 2: {}, Load Time 3: {}",
+            x, y, comp, loadTime1, loadTime2, loadTime3
         );
     }
 }
